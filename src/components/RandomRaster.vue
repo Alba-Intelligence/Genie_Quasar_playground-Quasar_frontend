acbd881fe326d10b7789e3856dcab90a39629563
Emmanuel-R8 <emmanuel.rialland@gmail.com>
2021-08-05 19:37:37 +0200

    Messing around

---
+++ src/components/RandomRaster.vue
@@ -0,0 +1,11 @@
+<template>
+
+<div>
+</div>
+
+<script>
+    import { getHttp } from '../http/fetchApi';
+</script>
+
+</template>
+
---
+++ src/http/baseFetch.ts
@@ -0,0 +1,29 @@
+// export default async (url, method, options = {}) => {
+//     let httpRequest;
+//     if (method.toUpperCase() === 'GET') {
+//         httpRequest = await fetch(url, {
+//             cache: 'reload',
+//             ...options,
+//         });
+//     } else {
+//         httpRequest = fetch(url, {
+//             method: method.toUpperCase(),
+//             cache: 'reload',
+//             ...options,
+//         });
+//     }
+//     return (type) => {
+//         switch (type.toLocaleLowerCase()) {
+//             case 'json':
+//                 return httpRequest.json();
+//             case 'blob':
+//                 return httpRequest.blob();
+//             case 'text':
+//                 return httpRequest.text();
+//             case 'formdata':
+//                 return httpRequest.formData();
+//             default:
+//                 return httpRequest.arrayBuffer();
+//         }
+//     };
+// };
